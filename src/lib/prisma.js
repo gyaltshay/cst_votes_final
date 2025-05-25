e9f619f46b4c3772591570d9b39399d8b1cf5990
shocolatte05 <02230146.cst@rub.edu.bt>
2025-05-25 22:13:12 +0600

    feat: push all changed codes

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -8,15 +8,6 @@ export const prisma = globalForPrisma.prisma || new PrismaClient({
     db: {
       url: process.env.DATABASE_URL
     }
-  },
-  // Configure connection pool
-  connection: {
-    pool: {
-      min: 2,
-      max: 10,
-      idleTimeoutMillis: 30000,
-      acquireTimeoutMillis: 30000,
-    }
   }
 });
 
